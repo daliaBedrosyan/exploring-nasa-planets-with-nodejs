81a7cda38ab62dbde1f85f49a1589273cd3bb7db
dalia9999 <daliabedrosyan47@gmail.com>
2025-04-30 22:39:59 +0300

    added stream event listeners for reading raw CSV file

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const parse = require('csv-parse');
+const { parse } = require('csv-parse');
+const fs = require('fs');
+
+const results = [];
+
+fs.createReadStream('kepler_data.csv')
+    .on('data', (chunk) => {
+        results.push(chunk);
+    })
+    .on('error', (error) => {
+        console.error(error);
+    })
+    .on('end', () => {
+        console.log(results);
+        console.log('done');
+    })
